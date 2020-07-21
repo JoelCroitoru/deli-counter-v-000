601a97b00a9384e15544845450ebc0d7c8be0f18
Joel Croitoru <jcroitoru@gmail.com>
2020-07-21 14:49:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,9 +19,9 @@ def take_a_number(katz_deli, name)
     return katz_deli
   end
 end
-def now_serving(katz_deli, name)
+def now_serving(katz_deli)
   if katz_deli.length > 0
-    puts "Currently serving #{name}."
+    puts "Currently serving Logan."
     katz_deli.shift
     return katz_deli
   else
